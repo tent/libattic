10107044cf52d3569e8341a07f96a6307fe6284b
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2012-11-19 14:57:11 -0500

    added vector based mutators

--- src/tentapp.h
+++ src/tentapp.h
@@ -39,27 +39,31 @@ public:
     std::string RequestAuthorizationURL();
     std::auto_ptr<AccessToken> RequestUserAuthorizationDetails(std::string szCode);
 
-    std::string GetAppID() const { return m_AppID; }
-    std::string GetAppName() const { return m_AppName; }
-    std::string GetAppDescription() const { return m_AppDescription; }
-    std::string GetAppHomepageURL() const { return m_AppHomepageURL; }
-    std::string GetAppIcon() const { return m_AppIcon; }
-    std::string GetMacAlgorithm() const { return m_MacAlgorithm; }
-    std::string GetMacKeyID() const { return m_MacKeyID; }
-    std::string GetMacKey() const { return m_MacKey; }
+    std::string GetAppID() const            { return m_AppID; }
+    std::string GetAppName() const          { return m_AppName; }
+    std::string GetAppDescription() const   { return m_AppDescription; }
+    std::string GetAppHomepageURL() const   { return m_AppHomepageURL; }
+    std::string GetAppIcon() const          { return m_AppIcon; }
+    std::string GetMacAlgorithm() const     { return m_MacAlgorithm; }
+    std::string GetMacKeyID() const         { return m_MacKeyID; }
+    std::string GetMacKey() const           { return m_MacKey; }
     
-    std::vector<std::string>* GetScopes() { return &m_Scopes; }
-    std::vector<std::string>* GetRedirectURIs() { return &m_RedirectURIs;}
-    std::vector<std::string>* GetAuthorizations() { return &m_Authorizations;}
+    std::vector<std::string>* GetScopes()           { return &m_Scopes; }
+    std::vector<std::string>* GetRedirectURIs()     { return &m_RedirectURIs;}
+    std::vector<std::string>* GetAuthorizations()   { return &m_Authorizations;}
     
-    void SetAppID(const std::string &szID) { m_AppID = szID; }
-    void SetAppName(const std::string &szName) { m_AppName = szName; }
-    void SetAppDescription(const std::string &szDescription) { m_AppDescription = szDescription; }
-    void SetAppHomepageURL(const std::string &szURL) { m_AppHomepageURL = szURL; }
-    void SetAppIcon(const std::string &szIcon) { m_AppIcon = szIcon; }
-    void SetMacAlgorithm(const std::string &szAlg) { m_MacAlgorithm = szAlg; }
-    void SetMacKeyID(const std::string &szID) { m_MacKeyID = szID; }
-    void SetMacKey(const std::string &szKey) { m_MacKey = szKey; }
+    void SetAppID(const std::string &szID)                      { m_AppID = szID; }
+    void SetAppName(const std::string &szName)                  { m_AppName = szName; }
+    void SetAppDescription(const std::string &szDescription)    { m_AppDescription = szDescription; }
+    void SetAppHomepageURL(const std::string &szURL)            { m_AppHomepageURL = szURL; }
+    void SetAppIcon(const std::string &szIcon)                  { m_AppIcon = szIcon; }
+    void SetMacAlgorithm(const std::string &szAlg)              { m_MacAlgorithm = szAlg; }
+    void SetMacKeyID(const std::string &szID)                   { m_MacKeyID = szID; }
+    void SetMacKey(const std::string &szKey)                    { m_MacKey = szKey; }
+
+    void SetScope(const std::string &szScope)           { m_Scopes.push_back(szScope); }
+    void SetRedirectURI(const std::string &szURI)       { m_RedirectURIs.push_back(szURI); }
+    void SetAuthorization(const std::string &szAuth)    { m_Authorizations.push_back(szAuth); }
 
 private:
     std::vector<std::string> m_Scopes;
