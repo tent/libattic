5b411671224aaa123b5146d80ca25cfc2b67391d
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2012-11-23 16:40:20 -0500

    cm wip

--- src/connectionmanager.h
+++ src/connectionmanager.h
@@ -1,5 +1,4 @@
 
-
 #ifndef CONNECTIONMANAGER_H_
 #define CONNECTIONMANAGER_H_
 #pragma once
@@ -20,8 +19,6 @@ class ConnectionManager
 
     void GenerateNonce(std::string &out);
     void GenerateHmacSha256(std::string &out);
-
-
 public:
 
     void BuildAuthHeader(const std::string &url, const std::string &requestMethod, const std::string &szMacID, const std::string &szMacKey, std::string &out);
@@ -37,6 +34,7 @@ public:
     static ConnectionManager* GetInstance();
 
     std::string HttpGet(std::string &url);  
+
     void HttpGetWithAuth(const std::string &szUrl, std::string &out, const std::string &szMacAlgorithm, const std::string &szMacID, const std::string &szMacKey, bool versbose = false);
 
     void HttpPost(const std::string &url, const std::string &body, std::string &responseOut, bool versbose = false);
@@ -45,12 +43,12 @@ public:
     void HttpPostWithAuth(const std::string &url, const std::string &body, std::string &responseOut, const std::string &szMacAlgorithm, const std::string &szMacID, const std::string &szMacKey, bool versbose = false);
 
     void ConnectionManager::HttpMultipartPost(const std::string &szUrl, const std::string &szBody, std::string &szFilePath, std::string &responseOut, const std::string &szMacAlgorithm, const std::string &szMacID, const std::string &szMacKey, bool versbose = false);
+
 private:
     CryptoPP::AutoSeededRandomPool  m_Rnd; // Random pool used for nonce(iv) generation
     static ConnectionManager *m_pInstance;
 
     CURL* m_pCurl;  // Curl instance
-
 };
 
 #endif
