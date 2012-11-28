6514d31e1384ed594056674a1e2d978e21b4acc1
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2012-11-28 12:34:36 -0500

    test fix wip

--- src/connectionmanager.cpp
+++ src/connectionmanager.cpp
@@ -583,61 +583,12 @@ void ConnectionManager::HttpMultipartPut( const std::string &url,
             
                 curl_multi_perform(multi_handle, &still_running);
 
-                do 
-                {
-                    struct timeval timeout;
-                    int rc; /* select() return code */ 
-                       
-                    fd_set fdread;
-                    fd_set fdwrite;
-                    fd_set fdexcep;
-                    int maxfd = -1;
-                                  
-                    long curl_timeo = -1;
-                                                     
-                    FD_ZERO(&fdread);
-                    FD_ZERO(&fdwrite);
-                    FD_ZERO(&fdexcep);
-                                              
-                    /* set a suitable timeout to play around with */ 
-                    timeout.tv_sec = 1;
-                    timeout.tv_usec = 0;
-                   
-                    curl_multi_timeout(multi_handle, &curl_timeo);
-                    if(curl_timeo >= 0)
-                    {
-                        timeout.tv_sec = curl_timeo / 1000;
-                        if(timeout.tv_sec > 1)
-                            timeout.tv_sec = 1;
-                        else
-                            timeout.tv_usec = (curl_timeo % 1000) * 1000;
-                    }
-                                                                                   
-                    /* get file descriptors from the transfers */ 
-                    curl_multi_fdset(multi_handle, &fdread, &fdwrite, &fdexcep, &maxfd);
-                     
-                    /* In a real-world program you OF COURSE check the return code of the
-                              function calls.  On success, the value of maxfd is guaranteed to be
-                              greater or equal than -1.  We call select(maxfd + 1, ...), specially in
-                            case of (maxfd == -1), we call select(0, ...), which is basically equal
-                      to sleep. */ 
-                     
-                    rc = select(maxfd+1, &fdread, &fdwrite, &fdexcep, &timeout);
-                                                                                                          
-                    switch(rc)
-                    {
-                        case -1:
-                            /* select error */ 
-                            break;
-                        case 0:
-                        default:
-                            /* timeout or readable/writable sockets */ 
-                            printf("perform!\n");
-                            curl_multi_perform(multi_handle, &still_running);
-                        printf("running: %d!\n", still_running);
-                        break;
-                    }
-                } while(still_running);
+int still_running=0;
+/* lets start the fetch */
+do {
+        while(::curl_multi_perform(multi_handle, &still_running) ==
+                        CURLM_CALL_MULTI_PERFORM);
+} while (still_running);
 
                 responseOut.clear();
                 responseOut.append(ExtractDataToString(s));
@@ -788,74 +739,12 @@ void ConnectionManager::HttpMultipartPost( const std::string &url,
             curl_easy_setopt(pCurl, CURLOPT_WRITEDATA, s); 
         
             curl_multi_perform(multi_handle, &still_running);
-
-            do 
-            {
-                
-                std::cout<< " here " << std::endl;
-                struct timeval timeout;
-                int rc; /* select() return code */ 
-                   
-                fd_set fdread;
-                fd_set fdwrite;
-                fd_set fdexcep;
-                int maxfd = -1;
-                              
-                long curl_timeo = -1;
-                                                 
-                FD_ZERO(&fdread);
-                FD_ZERO(&fdwrite);
-                FD_ZERO(&fdexcep);
-                                          
-                /* set a suitable timeout to play around with */ 
-                timeout.tv_sec = 1;
-                timeout.tv_usec = 0;
-               
-                curl_multi_timeout(multi_handle, &curl_timeo);
-                if(curl_timeo >= 0)
-                {
-                    timeout.tv_sec = curl_timeo / 1000;
-                    if(timeout.tv_sec > 1)
-                        timeout.tv_sec = 1;
-                    else
-                        timeout.tv_usec = (curl_timeo % 1000) * 1000;
-                }
-                                                                               
-                /* get file descriptors from the transfers */ 
-                CURLMcode mcode;
-                mcode = curl_multi_fdset( multi_handle, 
-                                         &fdread, 
-                                         &fdwrite, 
-                                         &fdexcep, 
-                                         &maxfd);
-                 
-                std::cout<< "mcode : " << mcode << std::endl;
-                /* In a real-world program you OF COURSE check the return code of the
-                          function calls.  On success, the value of maxfd is guaranteed to be
-                          greater or equal than -1.  We call select(maxfd + 1, ...), specially in
-                        case of (maxfd == -1), we call select(0, ...), which is basically equal
-                  to sleep. */ 
-                 
-                rc = select(maxfd+1, &fdread, &fdwrite, &fdexcep, &timeout);
-                                                                                                      
-                switch(rc)
-                {
-                    case -1:
-                        /* select error */ 
-                        std::cout<< "SELECT ERROR" << std::endl;
-                        break;
-                    case 0:
-                    default:
-                        /* timeout or readable/writable sockets */ 
-                        printf("perform!\n");
-                        CURLMcode mperf;
-                        mperf = curl_multi_perform(multi_handle, &still_running);
-
-                        std::cout<<"MPERF : " << mperf << std::endl;
-                    printf("running: %d!\n", still_running);
-                    break;
-                }
-            } while(still_running);
+int still_running=0;
+/* lets start the fetch */
+do {
+        while(::curl_multi_perform(multi_handle, &still_running) ==
+                        CURLM_CALL_MULTI_PERFORM);
+} while (still_running);
 
             std::cout<<"finished..."<<std::endl;
             responseOut.clear();
