67cb51697ac978e451884a900c8bf6e7e2057834
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2012-12-17 12:27:09 -0500

    crypto task, encrypts wip

--- src/encrypttask.cpp
+++ src/encrypttask.cpp
@@ -1,9 +1,22 @@
 #include "encrypttask.h"
 
-EncryptTask::EncryptTask( const std::string& filepath, const std::string& outpath )
+EncryptTask::EncryptTask( const std::string& filepath, 
+                          const std::string& outpath, 
+                          const Credentials* pCred, 
+                          bool generate)
+
 {
     m_Filepath = filepath;
     m_Outpath = outpath;
+
+    if(pCred)
+    {
+        // Copy credentials
+        m_Cred = *pCred;
+    }
+
+    m_Generate = generate;
+
 }
 
 EncryptTask::~EncryptTask()
@@ -13,14 +26,21 @@ EncryptTask::~EncryptTask()
 
 void EncryptTask::RunTask()
 {
+    int status = EncryptFile(m_Filepath, m_Outpath);
 
+    Callback(status, NULL);
 }
 
-
-void EncryptTask::RunTask()
+int EncryptTask::EncryptFile(const std::string& filepath, const std::string& outpath)
 {
+    if(m_Generate)
+    {
+        // TODO :: change this to pass by ref
+        m_Cred = GetCrypto()->GenerateCredentials();
+    }
 
+    int status = GetCrypto()->EncryptFile(filepath, outpath, m_Cred);
 
-//    Callback(status, NULL);
+    return status;
 }
 
--- src/encrypttask.h
+++ src/encrypttask.h
@@ -6,20 +6,28 @@
 #include <string>
 #include "cryptotask.h"
 
+
 class EncryptTask : public CryptoTask
 {
+    int EncryptFile(const std::string& filepath, const std::string& outpath);
 
 public:
-    EncryptTask( const std::string& filepath, const std::string& outpath);
+    EncryptTask( const std::string& filepath, 
+                 const std::string& outpath, 
+                 const Credentials* pCred = NULL, 
+                 bool generate=true);
 
     ~EncryptTask();
 
     void RunTask();
 
 private:
+    Credentials     m_Cred;
+
     std::string     m_Filepath;
     std::string     m_Outpath;
 
+    bool            m_Generate; // whether or not to generate credentials
 
 };
 
