5a11557d48e72f6ac343aa4b365a68c945568934
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2012-12-30 01:28:25 -0500

    updated const post type

--- src/constants.h
+++ src/constants.h
@@ -6,8 +6,16 @@
 
 namespace cnst
 {
+    // Post Types
+    static const char* g_szFileMetadataPostType = "https://cupcake.io/types/post/attic-file/v0.1.0";
+    static const char* g_szChunkStorePostType = "https://cupcake.io/types/post/attic-chunks/v0.1.0";
+    static const char* g_szFolderPostType = "https://cupcake.io/types/post/attic-folder/v0.1.0";
+
+    // TODO :: Remove, Depricated
     static const char* g_szAtticPostType = "https://tent.io/types/post/attic/v0.1.0";    
     static const char* g_szAtticMetaStorePostType = "https://tent.io/types/post/attic-metastore/v0.1.0";
+
+
     static const char* g_szAppData = "app";                                              
     static const char* g_szAuthToken = "at";                                             
     static const char* g_szManifest = "manifest";                                        
