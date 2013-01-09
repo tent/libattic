c8b90c3e6c53c87cc429be8412f57310d9b0d8af
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-01-09 17:03:51 -0500

    chunkinfo wip

--- src/chunkinfo.cpp
+++ src/chunkinfo.cpp
@@ -1,5 +1,7 @@
 #include "chunkinfo.h"
 
+#include <cbase64.h>
+
 ChunkInfo::ChunkInfo()
 {
 
@@ -19,16 +21,16 @@ ChunkInfo::~ChunkInfo()
 void ChunkInfo::Serialize(Json::Value& root)
 {
     root["chunk_name"] = m_ChunkName;
-    root["plaintext_mac"] = m_CheckSum;
-    root["ciphertext_mac"] = m_CipherTextChecksum;
-    root["iv"] = m_Iv;
+    root["plaintext_mac"] = cb64::base64_encode(reinterpret_cast<const unsigned char*>(m_CheckSum.c_str()), m_CheckSum.size());
+    root["ciphertext_mac"] = cb64::base64_encode(reinterpret_cast<const unsigned char*>(m_CipherTextChecksum.c_str()), m_CipherTextChecksum.size());
+    root["iv"] = cb64::base64_encode(reinterpret_cast<const unsigned char*>(m_Iv.c_str()), m_Iv.size());
 }
 
 void ChunkInfo::Deserialize(Json::Value& root)
 {
     m_ChunkName = root.get("chunk_name", "").asString();
-    m_CheckSum = root.get("plaintext_mac", "").asString();
-    m_CipherTextChecksum = root.get("ciphertext_mac", "").asString();
-    m_Iv = root.get("iv", "").asString();
+    m_CheckSum = cb64::base64_decode(root.get("plaintext_mac", "").asString());
+    m_CipherTextChecksum = cb64::base64_decode(root.get("ciphertext_mac", "").asString());
+    m_Iv = cb64::base64_decode(root.get("iv", "").asString());
 }
 
