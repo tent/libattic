ea663fccb697a290a1506cea61c671bfcd3bda71
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-01-28 20:59:45 -0500

    refactor set get for proper hmac (naming) and serialization deserialization

--- src/chunkinfo.cpp
+++ src/chunkinfo.cpp
@@ -21,16 +21,18 @@ ChunkInfo::~ChunkInfo()
 void ChunkInfo::Serialize(Json::Value& root)
 {
     root["chunk_name"] = m_ChunkName;
-    root["plaintext_mac"] = cb64::base64_encode(reinterpret_cast<const unsigned char*>(m_CheckSum.c_str()), m_CheckSum.size());
-    root["ciphertext_mac"] = cb64::base64_encode(reinterpret_cast<const unsigned char*>(m_CipherTextChecksum.c_str()), m_CipherTextChecksum.size());
+
+    root["plaintext_mac"] = cb64::base64_encode(reinterpret_cast<const unsigned char*>(m_PlainTextMac.c_str()), m_PlainTextMac.size());
+    root["ciphertext_mac"] = cb64::base64_encode(reinterpret_cast<const unsigned char*>(m_CipherTextMac.c_str()), m_CipherTextMac.size());
     root["iv"] = cb64::base64_encode(reinterpret_cast<const unsigned char*>(m_Iv.c_str()), m_Iv.size());
 }
 
 void ChunkInfo::Deserialize(Json::Value& root)
 {
     m_ChunkName = root.get("chunk_name", "").asString();
-    m_CheckSum = cb64::base64_decode(root.get("plaintext_mac", "").asString());
-    m_CipherTextChecksum = cb64::base64_decode(root.get("ciphertext_mac", "").asString());
+
+    m_PlainTextMac = cb64::base64_decode(root.get("plaintext_mac", "").asString());
+    m_CipherTextMac = cb64::base64_decode(root.get("ciphertext_mac", "").asString());
     m_Iv = cb64::base64_decode(root.get("iv", "").asString());
 }
 
--- src/chunkinfo.h
+++ src/chunkinfo.h
@@ -1,4 +1,3 @@
-
 #ifndef CHUNKINFO_H_
 #define CHUNKINFO_H_
 #pragma once
@@ -18,23 +17,26 @@ public:
     void Serialize(Json::Value& root);
     void Deserialize(Json::Value& root);
 
-    void GetChunkName(std::string& out) const               { out = m_ChunkName; }
-    void GetChecksum(std::string& out) const                { out = m_CheckSum; }
-    void GetCipherTextChecksum(std::string& out) const      { out = m_CipherTextChecksum; }
-    void GetIv(std::string& out) const                      { out = m_Iv; }
+    void GetChunkName(std::string& out) const       { out = m_ChunkName; }
+    void GetChecksum(std::string& out) const        { out = m_CheckSum; }
+    void GetPlainTextMac(std::string& out) const    { out = m_PlainTextMac; }
+    void GetCipherTextMac(std::string& out) const   { out = m_CipherTextMac; }
+    void GetIv(std::string& out) const              { out = m_Iv; }
 
     void SetChunkName(const std::string& name)      { m_ChunkName = name; } 
     void SetChecksum(const std::string& sum)        { m_CheckSum = sum; }
-    void SetCipherTextSum(const std::string& sum)   { m_CipherTextChecksum = sum; }
+    void SetPlainTextMac(const std::string& mac)    { m_PlainTextMac = mac; } 
+    void SetCipherTextMac(const std::string& mac)   { m_CipherTextMac = mac; }
     void SetIv(const std::string& iv)               { m_Iv = iv; }
 
     bool HasIv() const { return !m_Iv.empty(); }
 
 private:
-    std::string m_ChunkName;                // Name of this particular chunk
-    std::string m_CheckSum;                 // Hash of the chunk before encryption
-    std::string m_CipherTextChecksum;       // Hash of the Iv
-    std::string m_Iv;                       // Iv used to encrypt chunk
+    std::string m_ChunkName;        // Name of this particular chunk
+    std::string m_CheckSum;         // Hash of the chunk before encryption
+    std::string m_PlainTextMac;     // Hash of the chunk
+    std::string m_CipherTextMac;    // Hash of the Iv
+    std::string m_Iv;               // Iv used to encrypt chunk
 };
 
 #endif
