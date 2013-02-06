98e75498c8e304374970463d57901341af962319
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-02-06 16:27:03 -0500

    abstraction of upload, download, related tasks and enabling the possibility of more task specific data transforms and extraction

---
+++ src/uploadmanager.cpp
@@ -0,0 +1,134 @@
+#include "uploadmanager.h"
+
+#include "taskarbiter.h"
+#include "tentapp.h"
+#include "credentialsmanager.h"
+#include "filemanager.h"
+
+UploadManager::UploadManager( TentApp* pApp, 
+                              FileManager* pFm, 
+                              CredentialsManager* pCm,
+                              const AccessToken& at,
+                              const Entity& entity,
+                              const std::string& tempdir, 
+                              const std::string& workingdir,
+                              const std::string& configdir
+                            )
+{
+    m_pApp = pApp;
+    m_pFileManager = pFm;
+    m_pCredentialsManager = pCm;
+
+    m_AccessToken = at;
+    m_Entity = entity;
+
+    m_TempDir = tempdir;
+    m_WorkingDir = workingdir;
+    m_ConfigDir = configdir;
+}
+
+UploadManager::~UploadManager()
+{
+
+}
+
+int UploadManager::Initialize()
+{
+    int status = ret::A_OK;
+    status = m_TaskFactory.Initialize();
+
+    return status;
+}
+
+int UploadManager::Shutdown()
+{
+    int status = ret::A_OK;
+    status = m_TaskFactory.Shutdown();
+
+    return status;
+}
+
+void UploadManager::OnTaskCreate(Task* t)
+{
+    std::cout<<" On Task Create " << std::endl;
+    // Get Task type
+    // if pull or push task
+    // extract curl instance ptr
+}
+
+void UploadManager::OnTaskInsert(Task* t)
+{
+    std::cout<<" On Task Insert " << std::endl;
+
+    // Maybe move spin off task to spin off?
+    //status = TaskArbiter::GetInstance()->SpinOffTask(t);
+}
+
+int UploadManager::UploadFile(const std::string& filepath, void (*callback)(int, void*))
+{
+    int status = ret::A_OK;
+
+    Task* t = m_TaskFactory.SynchronousGetTentTask( Task::PUSH,
+                                                    m_pApp,
+                                                    m_pFileManager,
+                                                    m_pCredentialsManager,
+                                                    TaskArbiter::GetInstance(),
+                                                    &m_TaskFactory,
+                                                    m_AccessToken,
+                                                    m_Entity,
+                                                    filepath,
+                                                    m_TempDir,
+                                                    m_WorkingDir,
+                                                    m_ConfigDir,
+                                                    callback,
+                                                    this);
+
+    status = TaskArbiter::GetInstance()->SpinOffTask(t);
+    return status;
+}
+
+int UploadManager::DownloadFile(const std::string& filepath, void (*callback)(int, void*))
+{
+    int status = ret::A_OK;
+ 
+    Task* t = m_TaskFactory.SynchronousGetTentTask( Task::PULL,
+                                                    m_pApp,
+                                                    m_pFileManager,
+                                                    m_pCredentialsManager,
+                                                    TaskArbiter::GetInstance(),
+                                                    &m_TaskFactory,
+                                                    m_AccessToken,
+                                                    m_Entity,
+                                                    filepath,
+                                                    m_TempDir,
+                                                    m_WorkingDir,
+                                                    m_ConfigDir,
+                                                    callback,
+                                                    this);
+
+    status = TaskArbiter::GetInstance()->SpinOffTask(t);
+    return status;
+}
+
+int UploadManager::DownloadAllFiles(const std::string& filepath, void (*callback)(int, void*))
+{
+    int status = ret::A_OK;
+
+    return status;
+}
+
+int UploadManager::SyncFiles(const std::string& filepath, void (*callback)(int, void*))
+{
+    int status = ret::A_OK;
+
+    return status;
+}
+
+int UploadManager::DeleteFile(const std::string& filepath, void (*callback)(int, void*))
+{
+    int status = ret::A_OK;
+
+    return status;
+}
+
+
---
+++ src/uploadmanager.h
@@ -0,0 +1,60 @@
+#ifndef UPLOADMANAGER_H_
+#define UPLOADMANAGER_H_
+#pragma once
+
+#include <string>
+
+#include "accesstoken.h"
+#include "entity.h"
+#include "taskfactory.h"
+
+class TentApp;
+class FileManager;
+class CredentialsManager;
+
+class UploadManager : public TaskFactoryDelegate
+{
+public:
+    UploadManager( TentApp* pApp, 
+                   FileManager* pFm, 
+                   CredentialsManager* pCm,
+                   const AccessToken& at,
+                   const Entity& entity,
+                   const std::string& tempdir, 
+                   const std::string& workingdir,
+                   const std::string& configdir
+                 );
+
+    ~UploadManager();
+
+    int Initialize();
+    int Shutdown();
+
+
+    virtual void OnTaskCreate(Task* t);
+    virtual void OnTaskInsert(Task* t);
+
+    int UploadFile(const std::string& filepath, void (*callback)(int, void*));
+    int DownloadFile(const std::string& filepath, void (*callback)(int, void*));
+    int DownloadAllFiles(const std::string& filepath, void (*callback)(int, void*));
+    int SyncFiles(const std::string& filepath, void (*callback)(int, void*));
+    int DeleteFile(const std::string& filepath, void (*callback)(int, void*));
+
+
+private:
+    TaskFactory             m_TaskFactory; // Local to upload manager
+
+    TentApp*                m_pApp;
+    FileManager*            m_pFileManager;
+    CredentialsManager*     m_pCredentialsManager;
+
+    AccessToken             m_AccessToken;
+    Entity                  m_Entity;
+
+    std::string             m_TempDir;
+    std::string             m_WorkingDir;
+    std::string             m_ConfigDir;
+};
+
+#endif
+
