67ccb13538837b011ad0e0e87b30f5f2e52d9ed1
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-02-07 12:00:56 -0500

    added cmake

---
+++ third_party/c5/CMakeFiles/crypttest.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/crypttest.dir/bench.cpp.o"
+  "CMakeFiles/crypttest.dir/bench2.cpp.o"
+  "CMakeFiles/crypttest.dir/test.cpp.o"
+  "CMakeFiles/crypttest.dir/validat1.cpp.o"
+  "CMakeFiles/crypttest.dir/validat2.cpp.o"
+  "CMakeFiles/crypttest.dir/validat3.cpp.o"
+  "CMakeFiles/crypttest.dir/adhoc.cpp.o"
+  "CMakeFiles/crypttest.dir/datatest.cpp.o"
+  "CMakeFiles/crypttest.dir/regtest.cpp.o"
+  "CMakeFiles/crypttest.dir/fipsalgt.cpp.o"
+  "CMakeFiles/crypttest.dir/fipstest.cpp.o"
+  "crypttest.pdb"
+  "crypttest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/crypttest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ third_party/c5/cryptopp-config-build.cmake
@@ -0,0 +1,1 @@
+set (CRYPTOPP_INCLUDE_DIRS "/home/manuel/Projects/gt/tent/libattic/third_party/c5/..")
