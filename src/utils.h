0241e743e9bec880ad0050b5f2070604609fd954
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-02-23 17:59:03 -0500

    added hextostring method

--- src/utils.h
+++ src/utils.h
@@ -7,6 +7,8 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <stdexcept>
 
 #include <time.h>
 #include <stdlib.h>
@@ -108,6 +110,29 @@ namespace utils
             const char c = input[i];
             output.push_back(lut[c >> 4]);
             output.push_back(lut[c & 15]);
+        }
+    }
+
+    static void HexToString(const std::string& input, std::string& output)
+    {
+        static const char* const lut = "0123456789ABCDEF";
+        size_t len = input.length();
+
+        if (len & 1) throw std::invalid_argument("odd length");
+
+        output.clear();
+        output.reserve(len / 2);
+        for (size_t i = 0; i < len; i += 2)
+        {
+            char a = input[i];
+            const char* p = std::lower_bound(lut, lut + 16, a);
+            if (*p != a) throw std::invalid_argument("not a hex digit");
+
+            char b = input[i + 1];
+            const char* q = std::lower_bound(lut, lut + 16, b);
+            if (*q != b) throw std::invalid_argument("not a hex digit");
+
+            output.push_back(((p - lut) << 4) | (q - lut));
         }
     }
 
