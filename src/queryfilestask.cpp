2759b90dc31bac09854ca88887761e385de98be5
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-03-05 14:31:44 -0500

    updated queryfiletask.cpp

--- src/queryfilestask.cpp
+++ src/queryfilestask.cpp
@@ -35,30 +35,19 @@ int QueryFilesTask::CreateCStringListsAndCallBack(std::vector<FileInfo>& vec)
 {
     int status = ret::A_OK;
 
-    unsigned int totalcount = vec.size();
-
-    if(m_Stride == 0) m_Stride = 1;
-
-    int size = 0;
-    for(unsigned int i=0; i<totalcount; i+=m_Stride) {
-        if((i+m_Stride) < totalcount)
-            size = m_Stride;
-        else
-            size = (totalcount - i);
-        
-        char** buf = new char*[size];
-
+    unsigned int size = vec.size();
+    char** buf = new char*[size];
+    for(int j=0; j<size; j++) {
         std::string fp;
-        for(unsigned int j=0; j<size; j++) {
-           fp.clear(); 
-           vec[i+j].GetFilepath(fp); 
-           buf[j] = new char[fp.size()+1];
-           memset(buf[j], '\0', fp.size()+1);
-           memcpy(buf[j], fp.c_str(), fp.size());
-        }
-        Callback(status, buf, size, totalcount);
+        vec[j].GetFilepath(fp);
+
+        std::cout<< " pushing back : " << fp << std::endl; 
+        buf[j] = new char[fp.size()+1];
+        memset(buf[j], '\0', fp.size()+1);
+        memcpy(buf[j], fp.c_str(), fp.size());
     }
 
+    Callback(status, buf, size, size);
 }
 
 
