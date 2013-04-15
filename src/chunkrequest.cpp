42b1314812dc9c202a380868bf50b60459d9c716
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-04-14 22:06:06 -0400

    chunk request abstraction added

---
+++ src/chunkrequest.cpp
@@ -0,0 +1,148 @@
+#include "chunkrequest.h"
+
+#include "utils.h"
+#include "netlib.h"
+#include "errorcodes.h"
+#include "event.h"
+#include "jsonserializable.h"
+
+namespace attic { 
+
+ChunkRequest::ChunkRequest(const std::string& entity,
+                           const std::string& posts_feed, 
+                           const std::string& post_path, 
+                           const std::string& meta_post_id,
+                           const AccessToken& at) {
+    request_type_ = "POST"; // default
+    entity_ = entity;
+    access_token_ = at;
+    posts_feed_ = posts_feed;
+    post_path_ = post_path;
+    meta_post_id_ = meta_post_id;
+    url_ = posts_feed;
+    has_parent_ = false;
+    socket_ = NULL;
+}
+
+ChunkRequest::~ChunkRequest() {}
+
+void ChunkRequest::set_parent_post(const ChunkPost& cp) { 
+    parent_post_ = cp; 
+    has_parent_ = true;
+    url_.clear();
+    utils::FindAndReplace(post_path_, "{post}", parent_post_.id(), url_);
+    request_type_ = "PUT";
+}
+
+void ChunkRequest::BeginRequest() {
+    if(request_type_ == "PUT") {
+        Parent parent;
+        parent.version = parent_post_.version()->id;
+        post_.PushBackParent(parent);
+    }
+
+    std::string protocol, host, path;
+    netlib::ExtractHostAndPath(url_, protocol, host, path);
+    std::cout<<" Begining Chunk Request " << std::endl;
+    std::cout<<" protocol : " << protocol << std::endl;
+    std::cout<<" host : " << host << std::endl;
+    std::cout<<" path : " << path << std::endl;
+     
+    socket_ = new Connection(&io_service_);
+    socket_->Initialize(url_);
+
+    utils::GenerateRandomString(boundary_, 20);
+
+    // Build header request
+    boost::asio::streambuf request;
+    std::ostream request_stream(&request);
+    netlib::BuildRequestHeader(request_type_, url_, boundary_, &access_token_, request_stream); 
+        
+    // Start the request
+    socket_->Write(request);
+}
+
+int ChunkRequest::PushBackChunk(const std::string& chunk_name, 
+                                const std::string& chunk,
+                                const unsigned int count) { 
+    int status = ret::A_OK;
+
+    // Build Attachment
+    boost::asio::streambuf attachment;
+    std::ostream attachmentstream(&attachment);
+    netlib::BuildAttachmentForm(chunk_name, chunk, boundary_, count, attachmentstream);
+
+    boost::asio::streambuf part;
+    std::ostream chunkpartbuf(&part);
+    netlib::ChunkPart(attachment, chunkpartbuf);
+    status = WriteToSocket(part);
+
+    return status;
+}
+
+int ChunkRequest::WriteToSocket(boost::asio::streambuf& buffer) {
+    int breakcount = 0;
+    int retrycount = 20;
+
+    int status = ret::A_OK;
+    unsigned int buffersize = buffer.size();
+    for(unsigned int i=0; i < retrycount; i++) {
+        try {
+            status = ret::A_OK;
+            boost::timer::cpu_timer::cpu_timer t;
+            //size_t byteswritten = boost::asio::write(ssl_sock, buffer, errorcode); 
+            size_t byteswritten = socket_->Write(buffer);
+            std::cout<<" bytes written : " << byteswritten << std::endl;
+            boost::timer::cpu_times time = t.elapsed();
+            boost::timer::nanosecond_type elapsed = (time.system + time.user);
+
+            // To seconds
+            elapsed *= 0.0000000001; 
+            std::cout<<" ELAPSED : " << elapsed << std::endl;
+            if(elapsed > 0) {
+                unsigned int bps = (buffersize/elapsed);
+                // Raise event
+                char szSpeed[256] = {'\0'};
+                snprintf(szSpeed, 256, "%u", bps);
+                event::RaiseEvent(event::Event::UPLOAD_SPEED, std::string(szSpeed), NULL);
+            }
+            break;
+        }
+        catch(boost::system::system_error& err) {
+            std::cout<<" Write Socket error : " << err.what() << std::endl;
+            status = ret::A_FAIL_SOCKET_WRITE;
+        }
+    }
+
+    return status;
+}
+
+void ChunkRequest::EndRequest(Response& out) {
+    // Send chunk post body
+    std::string body;
+    jsn::SerializeObject(&post_, body);
+
+    boost::asio::streambuf requestBody;
+    std::ostream part_stream(&requestBody);
+    netlib::BuildBodyForm(post_.type(), body, boundary_, part_stream);
+    netlib::AddEndBoundry(part_stream, boundary_);
+
+    // Chunk the Body 
+    boost::asio::streambuf chunkedBody;
+    std::ostream partbuf(&chunkedBody);
+
+    netlib::ChunkEnd(requestBody, partbuf);
+    socket_->Write(chunkedBody);
+
+    std::cout<<" interpreting response " << std::endl;
+    socket_->InterpretResponse(out);
+
+    if(socket_) {
+        delete socket_;
+        socket_ = NULL;
+    }
+}
+
+
+} // namespace
+
---
+++ src/chunkrequest.h
@@ -0,0 +1,55 @@
+#ifndef CHUNKREQUEST_H_
+#define CHUNKREQUEST_H_
+#pragma once
+
+#include <string>
+
+#include "chunkpost.h"
+#include "accesstoken.h"
+#include "connection.h"
+#include "response.h"
+
+namespace attic { 
+
+class ChunkRequest {
+    int WriteToSocket(boost::asio::streambuf& buffer);
+public:
+    ChunkRequest(const std::string& entity,
+                 const std::string& posts_feed, 
+                 const std::string& post_path, 
+                 const std::string& meta_post_id,
+                 const AccessToken& at);
+
+    ~ChunkRequest();
+
+    void set_parent_post(const ChunkPost& cp);
+
+    void BeginRequest();
+    int PushBackChunk(const std::string& chunk_name, 
+                      const std::string& chunk,
+                      const unsigned int count);
+    void EndRequest(Response& out);
+private:
+    // TODO :: abstract this away, get a socket from connection manager somehow,
+    //         pass in a socket from the contructor, or something, use this for now.
+    boost::asio::io_service io_service_;
+    Connection* socket_;
+    std::string boundary_; // multipart boundary
+
+    ChunkPost post_;
+    ChunkPost parent_post_;
+    AccessToken access_token_;
+
+    std::string request_type_;
+    std::string posts_feed_;
+    std::string post_path_;
+    std::string meta_post_id_;
+    std::string url_;
+    std::string entity_;
+
+    bool has_parent_;
+};
+
+} // namespace
+#endif
+
