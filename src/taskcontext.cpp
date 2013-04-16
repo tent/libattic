1717ab69f1e2a33eba9d1e0dbdaee8cb8b109742
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-04-16 16:06:59 -0400

    marked get_value as const

--- src/taskcontext.cpp
+++ src/taskcontext.cpp
@@ -9,8 +9,8 @@ void TaskContext::set_value(const std::string& key, const std::string& value) {
     value_map_[key] = value;
 }
 
-bool TaskContext::get_value(const std::string& key, std::string& out) {
-    ValueMap::iterator itr = value_map_.find(key);
+bool TaskContext::get_value(const std::string& key, std::string& out) const { 
+    ValueMap::const_iterator itr = value_map_.find(key);
     if(itr != value_map_.end()) { 
         out = itr->second;
         return true;
--- src/taskcontext.h
+++ src/taskcontext.h
@@ -13,7 +13,7 @@ public:
     ~TaskContext();
 
     void set_value(const std::string& key, const std::string& value);
-    bool get_value(const std::string& key, std::string& out);
+    bool get_value(const std::string& key, std::string& out) const;
 private:
     typedef std::map<std::string, std::string> ValueMap;
     ValueMap value_map_;
