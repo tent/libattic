7285f818f25128294e371430ca96c715ecd8bf94
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-04-16 14:55:20 -0400

    code cleanup

--- src/postfilestrategy.h
+++ src/postfilestrategy.h
@@ -21,42 +21,6 @@ class PostFileStrategy : public HttpStrategyInterface {
 public:
     typedef std::map<unsigned int, ChunkPost> ChunkPostList; // key, chunk group
 private:
-    int DetermineChunkPostRequest(FileInfo* fi, 
-                                  Credentials& credOut, 
-                                  std::string& requestTypeOut,
-                                  std::string& urlOut,
-                                  std::string& postidOut);
-
-    int ProcessFile(const std::string& requestType,
-                    const std::string& url,
-                    const std::string& filepath,
-                    const Credentials& fileCredentials,
-                    FileInfo* pFi,
-                    Response& resp);
-
-    int ProcessChunk(const std::string& chunk, 
-                     const std::string& file_key, 
-                     const std::string& request_boundary,
-                     const unsigned int chunk_count,
-                     Connection& socket,
-                     FileInfo* fi,
-                     std::string& name_out);
-
-    int PrepareChunkPost(std::vector<std::string>& chunk_list, 
-                         ChunkPost& prev_post,
-                         FileInfo* fi, 
-                         ChunkPost& out);
-
-    int SendChunk(const std::string& chunk, 
-                  const std::string& chunk_name,
-                  const std::string& boundary,
-                  Connection& socket,
-                  const unsigned int count,
-                  bool end);
-
-    int WriteToSocket(Connection& socket,
-                      boost::asio::streambuf& buffer);
-
     int TransformChunk(const std::string& chunk, 
                        const std::string& fileKey,
                        std::string& finalizedOut, 
@@ -71,12 +35,6 @@ private:
 
     FileInfo* RetrieveFileInfo(const std::string& filepath);
 
-    int UpdateChunkPostMetadata(FileInfo* fi, 
-                                const Response& resp, 
-                                const Credentials& file_cred);
-
-    int SendFilePost( FileInfo* fi, const std::string& filepath);
-
     int InitializeFileMetaData(FileInfo* fi, const std::string& filepath, std::string& post_id_out);
 
     int RetrieveChunkPosts(const std::string& entity,
@@ -89,11 +47,8 @@ private:
                   ChunkPostList& chunk_list,
                   FileInfo::ChunkMap& chunk_map);
 
-
     void ExtractChunkInfo(ChunkPostList& list,
                           FileInfo::ChunkMap& out);
-
-    
 public:
     PostFileStrategy();
     ~PostFileStrategy();
