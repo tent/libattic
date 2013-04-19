c232fdafbd22496652da912747ef38de876a23fc
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-04-19 16:25:33 -0400

    removed unused variables

--- src/folderpost.h
+++ src/folderpost.h
@@ -19,17 +19,10 @@ public:
     virtual void Deserialize(Json::Value& root);
 
     const Folder& folder() const                { return folder_; }
-    const std::string& relative_path() const    { return relative_path_; }
-    const std::string& file_type() const        { return file_type_; }
-
     void set_folder(const Folder& folder)           { folder_ = folder; }
-    void set_relative_path(const std::string& path) { relative_path_ = path; }
-    void set_file_type(const std::string& type)     { file_type_ = type; }
 
 private:
     Folder  folder_;
-    std::string relative_path_; // Relative path to file or folder
-    std::string file_type_;     // File or Folder
 };
 
 } //namespace
