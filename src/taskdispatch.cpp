a0b9248449e6d6809de507a1b07956c48dd6076f
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-04-24 23:08:56 -0400

    created dispatch to pull from context queue in task manager, this runs on the service thread

---
+++ src/taskdispatch.cpp
@@ -0,0 +1,72 @@
+#include "taskdispatch.h"
+
+#include <iostream>
+
+#include "errorcodes.h"
+#include "taskfactory.h"
+#include "taskarbiter.h"
+#include "filemanager.h"
+
+
+namespace attic { 
+
+TaskDispatch::TaskDispatch(FileManager* fm,
+                           CredentialsManager* cm,
+                           const AccessToken& at,
+                           const Entity& entity,
+                           const std::string& tempdir, 
+                           const std::string& workingdir,
+                           const std::string& configdir) {
+    file_manager_ = fm;
+    credentials_manager_ = cm;
+
+    access_token_ = at;
+    entity_ = entity;
+
+    temp_directory_ = tempdir;
+    working_directory_ = workingdir;
+    config_directory_ = configdir;
+}
+
+TaskDispatch::~TaskDispatch() {
+    file_manager_ = NULL;
+    credentials_manager_ = NULL;
+}
+void TaskDispatch::Initialize() {}
+void TaskDispatch::Shutdown() {}
+
+
+void TaskDispatch::Process(TaskManager* tm) {
+    if(tm) {
+        tm->RetrieveContextQueue(hold_queue_);
+    }
+    // Copy n, number to process queue, then process that
+    dispatch_queue_ = hold_queue_; // TODO :: actually mitigate this properly, this is for testing
+    hold_queue_.clear();
+}
+
+void TaskDispatch::Dispatch() {
+    std::cout<<" DISPATCHING " << std::endl;
+    TaskContext::ContextQueue::iterator itr = dispatch_queue_.begin();
+    for(;itr != dispatch_queue_.end(); itr++) {
+        CreateAndSpinOffTask(*itr);
+    }
+}
+
+int TaskDispatch::CreateAndSpinOffTask(const TaskContext& tc) {
+    int status = ret::A_OK;
+    Task* t = task_factory_.GetTentTask(tc.type(),
+                                        file_manager_,
+                                        credentials_manager_,
+                                        access_token_,
+                                        entity_,
+                                        tc, 
+                                        tc.delegate(),
+                                        NULL);
+
+    status = TaskArbiter::GetInstance()->SpinOffTask(t);
+    return status;
+}
+
+} //namespace
+
---
+++ src/taskdispatch.h
@@ -0,0 +1,56 @@
+#ifndef TASKDISPATCH_H_
+#define TASKDISPATCH_H_
+#pragma once
+
+#include <string>
+#include "task.h"
+#include "tenttask.h"
+#include "taskcontext.h"
+#include "taskmanager.h"
+#include "filemanager.h"
+#include "taskfactory.h"
+#include "credentialsmanager.h"
+
+namespace attic { 
+
+class TaskDispatch {
+    int CreateAndSpinOffTask(const TaskContext& tc);
+public:
+    TaskDispatch(FileManager* fm,
+                 CredentialsManager* cm,
+                 const AccessToken& at,
+                 const Entity& entity,
+                 const std::string& tempdir, 
+                 const std::string& workingdir,
+                 const std::string& configdir);
+
+    ~TaskDispatch();
+
+    void Initialize();
+    void Shutdown();
+
+    void Process(TaskManager* tm);
+    void Dispatch();
+    
+    void set_dispatch_queue(const TaskContext::ContextQueue& q) { dispatch_queue_ = q; } 
+
+private:
+    TaskContext::ContextQueue dispatch_queue_;
+    TaskContext::ContextQueue hold_queue_;
+
+    // General settings for dispatch
+    TaskFactory             task_factory_;
+    FileManager*            file_manager_;
+    CredentialsManager*     credentials_manager_;
+
+    AccessToken             access_token_;
+    Entity                  entity_;
+
+    std::string             temp_directory_;
+    std::string             working_directory_;
+    std::string             config_directory_;
+};
+
+} //namespace
+#endif
+
