78bae95e1410e3fd5d8170702f859307f5195714
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-05-04 15:16:39 -0400

    implemented chunk encryption in chunk transform to abstract logic from task and provide common interface

--- src/chunktransform.cpp
+++ src/chunktransform.cpp
@@ -3,6 +3,8 @@
 #include "crypto.h"
 #include "compression.h"
 
+namespace attic {
+
 ChunkTransform::ChunkTransform(const std::string& chunk, const std::string& file_key) {
     data_ = chunk;
     file_key_ = file_key;
@@ -14,11 +16,20 @@ ChunkTransform::~ChunkTransform() {
 }
 
 void ChunkTransform::Transform() {
-
+    // Compress
+    std::string compressed_data;
+    Compress(data_, compressed_data);
+    // Encrypt
+    std::string encrypted_data;
+    Encrypt(compressed_data, encrypted_data);
+    // Encode
+    Encode(encrypted_data, finalized_data_);
+    // Generate verification hash
+    GenerateVerificationHash(verification_hash_);
 }
 
 void ChunkTransform::GenerateChunkName() {
-    if(!data.empty()) {
+    if(!data_.empty()) {
         crypto::GenerateHash(data_, plaintext_hash_);
         crypto::HexEncodeString(plaintext_hash_, name_);
     }
@@ -33,11 +44,19 @@ void ChunkTransform::Encrypt(const std::string& in, std::string& out) {
     crypto::GenerateIv(chunk_iv_);
     chunk_cred.set_iv(chunk_iv_);
     chunk_cred.set_key(file_key_);
-
     crypto::EncryptStringGCM(in, chunk_cred, out);
+    // generate ciphertext hash
+    crypto::GenerateHash(out, ciphertext_hash_);
 }
 
 void ChunkTransform::Encode(const std::string& in, std::string& out) {
-
+    crypto::Base64EncodeString(in, out);
 }
 
+void ChunkTransform::GenerateVerificationHash(std::string& out) {
+    std::string ver;
+    crypto::GenerateHexEncodedHmac(finalized_data_, ver);
+    out = ver.substr(0, 64);
+}
+
+} // namespace
--- src/chunktransform.h
+++ src/chunktransform.h
@@ -4,22 +4,28 @@
 
 #include <string>
 
+namespace attic {
+
 class ChunkTransform {
     void GenerateChunkName();
     void Compress(const std::string& in, std::string& out);
     void Encrypt(const std::string& in, std::string& out);
     void Encode(const std::string& in, std::string& out);
+    void GenerateVerificationHash(std::string& out);
 public:
     ChunkTransform(const std::string& chunk, const std::string& file_key);
     ~ChunkTransform();
 
     void Transform();
 
-    const std::string& file_key() const         { return file_key_; }
-    const std::string& name() const             { return name_; }
-    const std::string& data() const             { return data_; }
-    const std::string& finalized_data() const   { return finalized_data_; }
-    const std::string& chunk_iv() const         { return chunk_iv_; }
+    const std::string& file_key() const             { return file_key_; }
+    const std::string& chunk_iv() const             { return chunk_iv_; }
+    const std::string& name() const                 { return name_; }
+    const std::string& plaintext_hash() const       { return plaintext_hash_; }
+    const std::string& ciphertext_hash() const      { return ciphertext_hash_; }
+    const std::string& verification_hash() const    { return verification_hash_; }
+    const std::string& data() const                 { return data_; }
+    const std::string& finalized_data() const       { return finalized_data_; }
 private:
     // meta data
     std::string chunk_iv_;
@@ -27,11 +33,13 @@ private:
     std::string name_;
     std::string plaintext_hash_;
     std::string ciphertext_hash_;
+    std::string verification_hash_;
 
     // core data
     std::string data_;
     std::string finalized_data_;
 };
 
+} //namespace
 #endif
 
