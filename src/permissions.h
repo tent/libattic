701af88416d872a8f1e9e774c37806198208a7c2
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-05-07 15:20:02 -0400

    refactor wip

--- src/permissions.h
+++ src/permissions.h
@@ -1,4 +1,3 @@
-
 #ifndef PERMISSIONS_H_
 #define PERMISSIONS_H_
 #pragma once
@@ -10,92 +9,72 @@
 
 namespace attic { 
 
-class Group : public JsonSerializable
-{
+class Group : public JsonSerializable {
 public:
-    Group()
-    {
-        m_CreatedAt = 0;
+    Group() {
+        create_at_ = 0;
     }
     ~Group() {}
     
-     void Serialize(Json::Value& root)
-    {
-        root["id"] = m_Id;
-        root["created_at"] = m_CreatedAt;
-        root["name"] = m_Name;
+    void Serialize(Json::Value& root) {
+        root["id"] = id_;
+        root["created_at"] = create_at_;
+        root["name"] = name_;
     }
 
-    void Deserialize(Json::Value& root)
-    {
-        m_Id = root.get("id", "").asString();
-        m_CreatedAt = root.get("created_at", "").asInt();
-        m_Name = root.get("name", "").asString();
+    void Deserialize(Json::Value& root) {
+        id_ = root.get("id", "").asString();
+        create_at_ = root.get("created_at", "").asInt();
+        name_ = root.get("name", "").asString();
     }
 
 private:
-    std::string m_Id;
-    std::string m_Name;
-    int m_CreatedAt;
-
+    std::string id_;
+    std::string name_;
+    int create_at_;
 };
 
-
-class Permissions : public JsonSerializable
-{
+class Permissions : public JsonSerializable {
 public:
     typedef std::map<std::string, bool> EntityMap;
     typedef std::vector<Group> GroupList;
 
     Permissions() {
-        m_Public = false;
+        public_ = false;
     }
 
     ~Permissions() { } 
 
-    void Serialize(Json::Value& root)
-    {
-        root["public"] = m_Public;
-
+    void Serialize(Json::Value& root) {
+        root["public"] = public_;
         Json::Value groups;
-        for(unsigned int i=0; i< m_Groups.size(); i++)
-            jsn::SerializeObject(&m_Groups[i], groups );
-
+        for(unsigned int i=0; i< groups_.size(); i++)
+            jsn::SerializeObject(&groups_[i], groups );
         root["groups"] = groups;
-
         Json::Value entities(Json::arrayValue);
-        jsn::SerializeMapIntoObject(entities, m_EntityPermissionMap);
-
+        jsn::SerializeMapIntoObject(entities, entity_map_);
         root["entities"] = entities;
     }
 
-    void Deserialize(Json::Value& root)
-    {
-        m_Public = root.get("public", false).asBool();
-
-        if(root["groups"].isObject())
-        {
+    void Deserialize(Json::Value& root) {
+        public_ = root.get("public", false).asBool();
+        if(root["groups"].isObject()) {
             Json::ValueIterator itr = root["groups"].begin();
-
-            for(; itr != root["groups"].end(); itr++)
-            {
+            for(; itr != root["groups"].end(); itr++) {
                 Group g;
                 jsn::DeserializeObject(&g, *itr);
-                m_Groups.push_back(g);
+                groups_.push_back(g);
             }
-
         }
-
-        jsn::DeserializeObjectValueIntoMap(root["entities"], m_EntityPermissionMap);
+        jsn::DeserializeObjectValueIntoMap(root["entities"], entity_map_);
     }
 
-    bool GetIsPublic() const { return m_Public; }
-    void SetIsPublic(const bool pub) { m_Public = pub; }
-
+    bool GetIsPublic() const { return public_; }
+    void SetIsPublic(const bool pub) { public_ = pub; }
 private:
-    EntityMap m_EntityPermissionMap;
-    GroupList m_Groups;
-    bool    m_Public;
+    EntityMap entity_map_;
+    GroupList groups_;
+    bool    public_;
 };
 
 }//namespace
