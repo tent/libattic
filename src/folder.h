96cf9242ed989c7f44d832aba1bda90c1fb8b52e
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-05-15 20:50:45 -0400

    removed manifest id, added parent id

--- src/folder.h
+++ src/folder.h
@@ -12,16 +12,16 @@ public:
     ~Folder() {}
 
     const std::string& folderpath() const       { return folderpath_; }
-    const std::string& manifest_id() const      { return manifest_id_; }
     const std::string& folder_post_id() const   { return folder_post_id_; }
+    const std::string& parent_post_id() const   { return parent_post_id_; } 
 
     void set_folderpath(const std::string& path)    { folderpath_ = path; } 
-    void set_manifest_id(const std::string& id)     { manifest_id_ = id; }
     void set_folder_post_id(const std::string& id)  { folder_post_id_ = id; } 
+    void set_parent_post_id(const std::string& id)  { parent_post_id_ = id; }
 private:
     std::string folderpath_;
-    std::string manifest_id_;
     std::string folder_post_id_;
+    std::string parent_post_id_;
 };
 
 }//namespace
