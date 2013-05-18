99709ed052a56b6cbd61758edd2ddfeb665ab527
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-05-18 14:59:53 -0400

    created table handler interface, abstracting sqlite table queries

---
+++ src/tablehandler.cpp
@@ -0,0 +1,159 @@
+#include "tablehandler.h"
+
+namespace attic { 
+
+TableHandler::TableHandler(sqlite3* db, const std::string& name) {
+    db_ = db;
+    stmt_ = NULL;
+    table_name_ = name;
+}
+
+TableHandler::~TableHandler() {
+    db_ = NULL;
+    stmt_ = NULL;
+}
+
+bool TableHandler::Exec(const std::string& query, std::string& error_out) const {
+    if(!db_ || query.empty())
+        return false;
+
+    char* szError = NULL;
+    int rc = sqlite3_exec(db_, 
+                          query.c_str(), 
+                          NULL, 
+                          NULL,
+                          &szError);
+
+    if(rc != SQLITE_OK) {
+        if(szError) {
+            error_out.append(szError);
+            sqlite3_free(szError);
+            szError = NULL;
+        }
+        return false;
+    }
+    return true;
+}
+
+bool TableHandler::Select(const std::string& select, SelectResult& out, std::string& error_out) const {
+    if(!db_ || select.empty())
+        return false;
+
+    char *szError = NULL;
+    int rc = sqlite3_get_table(db_,
+                               select.c_str(),     /* SQL statement to be evaluated */
+                               &out.results_,      /* Results of the query */
+                               &out.row_,          /* Number of result rows written here */
+                               &out.col_,          /* Number of result columns written here */
+                               &szError);          /* Error msg written here, get table method handles cleanup */
+    if(rc != SQLITE_OK) {
+        if(szError)
+            error_out.append(szError);
+        return false;
+    }
+
+    return true;
+}
+
+bool TableHandler::PrepareStatement(const std::string& statement, std::string& error_out) {
+    sqlite3_stmt* stmt = NULL;
+    int ret = sqlite3_prepare_v2(db_, statement.c_str(), -1, &stmt, 0);
+
+    if(ret != SQLITE_OK) {
+        error_out = "Failed to prepare statement";
+        stmt = NULL;
+        return false;
+    }
+    stmt_ = stmt;
+    return true;
+}
+
+bool TableHandler::StepStatement(std::string& error_out) {
+    if(stmt_) {
+        int ret = sqlite3_step(stmt_);
+        if(ret != SQLITE_DONE) {
+            error_out += "Step, Error Message : ";
+            error_out += sqlite3_errmsg(db_);
+            error_out += "\n";
+        }
+        else  {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool TableHandler::FinalizeStatement(std::string& error_out) {
+    if(stmt_) {
+        int ret = sqlite3_finalize(stmt_);
+        if(ret != SQLITE_OK) {
+            error_out += "Finalize Error Message : ";
+            error_out +=  sqlite3_errmsg(db_);
+            error_out += "\n";
+        }
+        else {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool TableHandler::BindInt(unsigned int position, int value, std::string& error_out) {
+    if(stmt_) {
+        int ret = sqlite3_bind_int(stmt_, position, value);
+        if(ret != SQLITE_OK) {
+            error_out += "Bind Int Error, position : ";
+            error_out += position;
+            error_out += " Error Message : ";
+            error_out +=  sqlite3_errmsg(db_);
+            error_out += "\n";
+        }
+        else {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool TableHandler::BindText(unsigned int position, std::string& value, std::string& error_out) {
+    if(stmt_) {
+        int ret = sqlite3_bind_text(stmt_, position, value.c_str(), value.size(), SQLITE_STATIC);
+        if(ret != SQLITE_OK) {
+            error_out += "Bind Text Error, position : ";
+            error_out += position;
+            error_out += " Error message: ";
+            error_out +=  sqlite3_errmsg(db_);
+            error_out += "\n";
+        }
+        else {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool TableHandler::BindBlob(unsigned int position, std::string& value, std::string& error_out) {
+    if(stmt_) {
+        int ret = sqlite3_bind_blob(stmt_, position, value.c_str(), value.size(), SQLITE_TRANSIENT);
+        if(ret != SQLITE_OK) {
+            error_out += "Bind Blob Error, position : ";
+            error_out += position;
+            error_out += " Error message: ";
+            error_out +=  sqlite3_errmsg(db_);
+            error_out += "\n";
+        }
+        else {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool TableHandler::ClearStatement() {
+    // TODO :: probably some other step with clearing a non finalized statement, look it up at some point
+    stmt_ = NULL;
+}
+
+
+} // namespace
+
---
+++ src/tablehandler.h
@@ -0,0 +1,58 @@
+#ifndef TABLEHANDLER_H_
+#define TABLEHANDLER_H_
+#pragma once
+
+#include <string>
+#include <sqlite3.h>
+
+namespace attic {
+
+class SelectResult { 
+    friend class Manifest;
+    friend class TableHandler;
+public:
+    SelectResult() {
+        row_ = 0;
+        col_ = 0;
+    }
+    ~SelectResult() {
+        sqlite3_free_table(results_);
+    }
+
+    std::string operator[](const unsigned int n) { return results_[n]; }
+
+    int row() const { return row_; }
+    int col() const { return col_; }
+private:
+    char** results_;
+    int row_;
+    int col_;
+};
+
+class TableHandler {
+public:
+    TableHandler(sqlite3* db, const std::string& name);
+    virtual ~TableHandler();
+    virtual bool CreateTable() = 0;
+    bool Exec(const std::string& query, std::string& error_out) const;
+    bool Select(const std::string& select, SelectResult& out, std::string& error_out) const;
+
+    const std::string& table_name() const { return table_name_; }
+
+    bool PrepareStatement(const std::string& statement, std::string& error_out);
+    bool StepStatement(std::string& error_out);
+    bool FinalizeStatement(std::string& error_out);
+    bool BindInt(unsigned int position, int value, std::string& error_out);
+    bool BindText(unsigned int position, std::string& value, std::string& error_out);
+    bool BindBlob(unsigned int position, std::string& value, std::string& error_out);
+
+    bool ClearStatement();
+private:
+    sqlite3* db_;
+    sqlite3_stmt* stmt_;
+    std::string table_name_;
+};
+
+}//namespace
+#endif
+
