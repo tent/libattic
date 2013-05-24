647863434306493104295259ee2e39b9e835d565
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-05-24 15:05:22 -0400

    fleshed out the basic attic service object

--- src/atticservice.cpp
+++ src/atticservice.cpp
@@ -11,8 +11,13 @@ namespace attic {
 AtticService::AtticService() {
     service_manager_ = NULL;
     task_manager_ = NULL;
+    task_arbiter_ = NULL;
     thread_manager_ = NULL;
+    file_manager_ = NULL;
+    credentials_manager_ = NULL;
     client_ = NULL;
+
+    running_ = false;
 }
 
 AtticService::~AtticService() {
@@ -20,10 +25,36 @@ AtticService::~AtticService() {
 }
 
 int AtticService::start() {
+    int status = ret::A_OK;
     utils::SeedRand();
     LoadConfigValues();
-    ValidateDirectories();
+    status = ValidateDirectories();
+    if(status == ret::A_OK){
+        // Initialize File Manager
+        status = InitializeFileManager();           if(status != ret::A_OK) return status;
+        status = InitializeCredentialsManager();    if(status != ret::A_OK) return status;
+        status = InitializeClient();                if(status != ret::A_OK) return status;
+        status = InitializeTaskManager();           if(status != ret::A_OK) return status;
+        status = InitializeServiceManager();        if(status != ret::A_OK) return status;
+        status = InitializeTaskArbiter();           if(status != ret::A_OK) return status;
+        status = InitializeThreadManager();         if(status != ret::A_OK) return status;
+    }
+    running_ = true;
+    return status;
+}
 
+int AtticService::stop() {
+    int status = ret::A_OK;
+    status = ShutdownThreadManager();
+    status = ShutdownTaskManager();
+    status = ShutdownServiceManager();
+    status = ShutdownTaskManager();
+    status = ShutdownClient();
+    status = ShutdownCredentialsManager();
+    status = ShutdownFileManager();
+    ConfigManager::GetInstance()->Shutdown();
+    running_ = false;
+    return status;
 }
 
 void AtticService::LoadConfigValues() {
@@ -33,17 +64,178 @@ void AtticService::LoadConfigValues() {
     ConfigManager::GetInstance()->GetValue(cnst::g_szConfigEntityURL, entity_url_);
 }
 
-void AtticService::ValidateDirectories() {
-    if(!working_dir_.empty())
+int AtticService::ValidateDirectories() {
+    int status = ret::A_OK;
+    if(!working_dir_.empty() && !config_dir_.empty() && !temp_dir_.empty()) {
+        // TODO:: make sure these are all absolute paths
         fs::CreateDirectory(working_dir_);
-    if(!config_dir_.empty())
         fs::CreateDirectory(config_dir_);
-    if(!temp_dir_.empty())
         fs::CreateDirectory(temp_dir_);
+    }
+    else {
+        status = ret::A_FAIL_VALIDATE_DIRECTORY;
+    }
+    return status;
 }
 
-int AtticService::stop() {
-
+int AtticService::InitializeFileManager() {
+    int status = ret::A_OK;
+    if(!file_manager_) {
+        file_manager_ = new FileManager();
+        status = file_manager_->Initialize(config_dir_,
+                                           working_dir_,
+                                           temp_dir_);
+    }
+    return status;
 }
+
+// TODO :: REFACTOR get rid of credentials manager
+int AtticService::InitializeCredentialsManager() {
+    int status = ret::A_OK;
+    if(!credentials_manager_) {
+        credentials_manager_ = new CredentialsManager();
+        credentials_manager_->SetConfigDirectory(config_dir_);
+        status = credentials_manager_->Initialize();
+        if(status == ret::A_OK) {
+            status = credentials_manager_->LoadAccessToken();
+        }
+    }
+    return status;
+}
+
+int AtticService::InitializeClient() {
+    int status = ret::A_OK;
+    AccessToken at;
+    credentials_manager_->GetAccessTokenCopy(at);
+    client_ = new Client(working_dir_,
+                         config_dir_,
+                         temp_dir_,
+                         entity_url_,
+                         at);
+    status = client_->Initialize();
+    return status;
+}
+
+int AtticService::InitializeTaskManager() {
+    int status = ret::A_OK;
+    if(!task_manager_) {
+        AccessToken at;
+        credentials_manager_->GetAccessTokenCopy(at);
+        task_manager_ = new TaskManager(file_manager_, 
+                                        credentials_manager_, 
+                                        at, 
+                                        client_->entity(),
+                                        temp_dir_,
+                                        working_dir_,
+                                        config_dir_);
+        status = task_manager_->Initialize();
+    }
+    return status;
+}
+
+int AtticService::InitializeServiceManager() {
+    int status = ret::A_OK;
+    if(!service_manager_) {
+        service_manager_ = new ServiceManager(task_manager_);
+        status = service_manager_->Initialize();
+    }
+    return status;
+}
+
+int AtticService::InitializeTaskArbiter() {
+    int status = ret::A_OK;
+    if(!task_arbiter_) {
+        task_arbiter_ = TaskArbiter::GetInstance();
+        status = task_arbiter_->Initialize(task_manager_);
+    }
+    return status;
+}
+
+int AtticService::InitializeThreadManager() {
+    int status = ret::A_OK;
+    if(!thread_manager_) {
+        AccessToken at;
+        credentials_manager_->GetAccessTokenCopy(at);
+        thread_manager_ = new ThreadManager(file_manager_,
+                                            credentials_manager_,
+                                            at,
+                                            client_->entity());
+        // TODO :: setup a configurable way to set the thread count
+        status = thread_manager_->Initialize(12); 
+    }
+    return status;
+}
+
+int AtticService::ShutdownThreadManager() {
+    int status = ret::A_OK;
+    if(thread_manager_) {
+        status = thread_manager_->Shutdown();
+        delete thread_manager_;
+        thread_manager_ = NULL;
+    }
+    return status;
+}
+
+int AtticService::ShutdownTaskArbiter() {
+    int status = ret::A_OK;
+    if(task_arbiter_) {
+        // NOTE* since this is a singleton it deletes itself on shutdown
+        // TODO :: eventually make the task arbiter not a singleton
+        status = task_arbiter_->Shutdown();
+        task_arbiter_ = NULL;
+    }
+    return status;
+}
+
+int AtticService::ShutdownServiceManager() {
+    int status = ret::A_OK;
+    if(service_manager_) {
+        status = service_manager_->Shutdown();
+        delete service_manager_;
+        service_manager_ = NULL;
+    }
+    return status;
+}
+
+int AtticService::ShutdownTaskManager() {
+    int status = ret::A_OK;
+    if(task_manager_) {
+        task_manager_->Shutdown();
+        delete task_manager_;
+        task_manager_ = NULL;
+    }
+    return status;
+}
+
+int AtticService::ShutdownClient() {
+    int status = ret::A_OK;
+    if(client_) {
+        client_->Shutdown();
+        delete client_;
+        client_ = NULL;
+    }
+    return status;
+}
+
+int AtticService::ShutdownCredentialsManager() {
+    int status = ret::A_OK;
+    if(credentials_manager_) {
+        status = credentials_manager_->Shutdown();
+        delete credentials_manager_;
+        credentials_manager_ = NULL;
+    }
+    return status;
+}
+
+int AtticService::ShutdownFileManager() {
+    int status = ret::A_OK;
+    if(file_manager_) {
+        status = file_manager_->Shutdown();
+        delete file_manager_;
+        file_manager_ = NULL;
+    }
+    return status;
+}
+
 
 }// namespace
--- src/atticservice.h
+++ src/atticservice.h
@@ -7,13 +7,32 @@
 #include "atticclient.h"
 #include "servicemanager.h"
 #include "taskmanager.h"
+#include "taskarbiter.h"
 #include "threading.h"
+#include "filemanager.h"
+#include "credentialsmanager.h"
 
 namespace attic {
 
 class AtticService {
     void LoadConfigValues();
-    void ValidateDirectories();
+    int ValidateDirectories();
+    // Order does matter for init and shutdown sequence
+    int InitializeFileManager();
+    int InitializeCredentialsManager();
+    int InitializeClient();
+    int InitializeTaskManager();
+    int InitializeServiceManager();
+    int InitializeTaskArbiter();
+    int InitializeThreadManager();
+
+    int ShutdownThreadManager();
+    int ShutdownTaskArbiter();
+    int ShutdownServiceManager();
+    int ShutdownTaskManager();
+    int ShutdownClient();
+    int ShutdownCredentialsManager();
+    int ShutdownFileManager();
 public:
     AtticService();
     ~AtticService();
@@ -33,16 +52,25 @@ public:
     int EnterPassphrase();
     int ChangePassphrase();
     int EnterRecoveryKey();
+
+    bool running()                              { return running_; }
+    TaskManager* task_manager()                 { return task_manager_; }
+    CredentialsManager* credentials_manager()   { return credentials_manager_; }
+    Client* client()                            { return client_; }
 private:
     bool running_;
 
     ServiceManager*     service_manager_;
     TaskManager*        task_manager_;
+    TaskArbiter*        task_arbiter_;
     ThreadManager*      thread_manager_;
+    FileManager*        file_manager_;
+    CredentialsManager* credentials_manager_;
     Client*             client_;
 
     CallbackHandler callback_handler_;
 
+    // File paths should all be absolute paths
     std::string working_dir_;
     std::string config_dir_;
     std::string temp_dir_;
