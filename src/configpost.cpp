110416eb78e70f4e6da4610ef557febb7a05f165
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-06-17 23:32:05 -0400

    serialization and deserialization into json

--- src/configpost.cpp
+++ src/configpost.cpp
@@ -14,9 +14,13 @@ ConfigPost::~ConfigPost() {
 
 void ConfigPost::Serialize(Json::Value& root) {
     Json::Value config(Json::objectValue);
-    std::map<std::string, std::string>::iterator itr = config_map_.begin();
+    std::map<std::string, ConfigValue>::iterator itr = config_map_.begin();
     for(;itr!= config_map_.end(); itr++) {
-        config[itr->first] = itr->second;
+        Json::Value config_val(Json::objectValue);
+        config_val["type"] = itr->second.type;
+        config_val["key"] = itr->second.key;
+        config_val["value"] = itr->second.value;
+        config[itr->first] = config_val;
     }
     set_content("config", config);
 
@@ -32,14 +36,24 @@ void ConfigPost::Deserialize(Json::Value& root) {
     config_map_.clear();
     Json::ValueIterator itr = config.begin();
     for(;itr!= config.end(); itr++) {
-        std::cout<<" key : " << itr.key().asString() << std::endl;
-        std::cout<<" val : " << (*itr).asString() << std::endl;
-        config_map_[itr.key().asString()] = (*itr).asString();
+        Json::Value config_value(Json::objectValue);
+        config_value = *itr;
+        ConfigValue v;
+        v.type = config_value.get("type", "").asString();
+        v.key = config_value.get("key", "").asString();
+        v.value = config_value.get("value", "").asString();
+        config_map_[v.key] = v;
     }
 }
 
-void ConfigPost::PushBackConfigValue(const std::string& key, const std::string& value) {
-    config_map_[key] = value;
+void ConfigPost::PushBackConfigValue(const std::string& type,
+                                     const std::string& key, 
+                                     const std::string& value) {
+    ConfigValue v;
+    v.type = type;
+    v.key = key;
+    v.value = value;
+    config_map_[key] = v;
 }
 
 }//namespace
--- src/configpost.h
+++ src/configpost.h
@@ -3,24 +3,35 @@
 #pragma once
 
 #include <string>
-#include <map>
+#include <deque>
 
 #include <post.h>
 
 namespace attic { 
 
+struct ConfigValue {
+    std::string type;
+    std::string key;
+    std::string value;
+};
+
 class ConfigPost : public Post {
 public:
+
     ConfigPost();
     ~ConfigPost();
 
     virtual void Serialize(Json::Value& root);  
     virtual void Deserialize(Json::Value& root);
 
-    void PushBackConfigValue(const std::string& key, const std::string& value);
+    void PushBackConfigValue(const std::string& type, 
+                             const std::string& key, 
+                             const std::string& value);
+
+    std::map<std::string, ConfigValue>* config_map() { return &config_map_; }
 
 private:
-    std::map<std::string, std::string> config_map_;
+    std::map<std::string, ConfigValue> config_map_;
 };
 
 }// namespace
