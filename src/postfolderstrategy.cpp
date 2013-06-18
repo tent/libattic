add04f124be4c2260d189031d3ab66e38ba160f1
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-06-17 23:35:19 -0400

    updated folder creation upon push file to work with new folder layout

--- src/postfolderstrategy.cpp
+++ src/postfolderstrategy.cpp
@@ -25,47 +25,51 @@ int PostFolderStrategy::Execute(FileManager* pFileManager, CredentialsManager* p
         //Extract Parent Directory
         std::string folderpath;
         status = fs::GetParentPath(filepath, folderpath);
+        std::cout<<" FOLDER PATH : " << folderpath << std::endl;
         if(!folderpath.empty()) {
-            std::deque<Folder> folder_list;
-            FolderHandler fh(file_manager_);
-            
-            if(fh.RetrieveFolders(folderpath, folder_list)){
-                // Folder list comes out child -> parent in that order
-
-                // top level folder's parent is working directory
-                std::deque<Folder>::iterator itr = folder_list.end();
-
-                std::string hold_id = cnst::g_szWorkingPlaceHolder; 
-                while(itr!= folder_list.begin()) {
-                    --itr;
-                    // Set Parent post id;
-                    if((*itr).parent_post_id().empty())    
-                        (*itr).set_parent_post_id(hold_id);
-                    // Create new folder post
-                    if((*itr).folder_post_id().empty()) {
-                        status = CreateFolderPost(*itr, hold_id); 
-                        if(status == ret::A_OK) {
-                            fh.InsertFolder(*itr);
-                            hold_id = (*itr).folder_post_id();
+            std::string directory, directory_post_id;
+            if(file_manager_->FindAssociatedWorkingDirectory(folderpath, 
+                                                             directory, 
+                                                             directory_post_id)) {
+                FolderHandler fh(file_manager_);
+                std::deque<std::string> names;
+                if(fh.RetrieveFolders(folderpath, directory, names)) {
+                    // validate each folder exists
+                    std::deque<std::string>::iterator itr = names.begin();
+                    std::string parent_post_id = directory_post_id;
+                    for(;itr!=names.end();itr++) {
+                        Folder folder;
+                        if(!file_manager_->GetFolderEntry((*itr), parent_post_id, folder)) {
+                            folder.set_foldername(*itr);
+                            folder.set_parent_post_id(parent_post_id);
+                            //  if not create post
+                            std::string post_id;
+                            CreateFolderPost(folder, post_id);
+                            // Insert to table;
+                            file_manager_->CreateFolderEntry(folder.foldername(),
+                                                              folder.folder_post_id(),
+                                                              folder.parent_post_id(),
+                                                              folder);
                         }
+                        else {
+                            // Check if folderpath is deleted
+                            if(file_manager_->IsFolderDeleted(folder.folder_post_id())){
+                                // Un-delete
+                                file_manager_->SetFolderDeleted(folder.folder_post_id(), false);
+                                UpdateFolderPost(folder, folder.folder_post_id());
+                            }
+                        }
+                        parent_post_id = folder.folder_post_id();
                     }
-                    else { 
-                        // Post already exists
-                        // set id for parent
-                        hold_id = (*itr).folder_post_id();
-                    }
+                    
                 }
             }
-            /*
-            else {
-                status = ret::A_FAIL_CREATE_DIRECTORY;
-            }
-            */
         }
     }
     else {
         status = ret::A_FAIL_INVALID_FILEPATH;
     }
+    std::cout<<" POST FODLER STRATEGY STATUS = " << status << std::endl;
 
     return status;
 }
@@ -86,5 +90,61 @@ int PostFolderStrategy::CreateFolderPost(Folder& folder, std::string& id_out) {
     }
     return status;
 }
+
+bool PostFolderStrategy::UpdateFolderPost(Folder& folder, const std::string post_id) {
+    bool ret = false;
+    if(!post_id.empty()) {
+        FolderPost fp;
+        if(RetrieveFolderPost(folder.folder_post_id(), fp) == ret::A_OK) {
+            fp.set_folder(folder);
+            if(PostFolderPost(post_id, fp) == ret::A_OK)
+                ret = true;
+        }
+    }
+    else {
+        std::ostringstream err;
+        err << " Empty Folder Post id : " << std::endl;
+        err << " entry : " << folder.foldername();
+        err << " \t\t " << folder.folder_post_id();
+        err << " \t\t " << folder.parent_post_id();
+        log::LogString("pfs_18912512", err.str());
+    }
+    return ret;
+}
+
+int PostFolderStrategy::RetrieveFolderPost(const std::string& post_id, FolderPost& out) {
+    int status = ret::A_OK;
+    if(!post_id.empty()) {
+        std::string post_path = post_path_;
+        std::string posturl;
+        utils::FindAndReplace(post_path, "{post}", post_id, posturl);
+        PostHandler<FolderPost> ph(access_token_);
+        status = ph.Get(posturl, NULL, out);
+        if(status != ret::A_OK)
+            log::LogHttpResponse("pfs_13581", ph.response());
+    }
+    else { 
+        status = ret::A_FAIL_INVALID_POST_ID;
+    }
+    return status;
+}
+
+int PostFolderStrategy::PostFolderPost(const std::string& post_id, FolderPost& fp) {
+    int status = ret::A_OK;
+    if(!post_id.empty()) {
+        std::string post_path = post_path_;
+        std::string posturl;
+        utils::FindAndReplace(post_path, "{post}", post_id, posturl);
+        PostHandler<FolderPost> ph(access_token_);
+        status = ph.Put(posturl, NULL, fp);
+        if(status != ret::A_OK)
+            log::LogHttpResponse("pfs_n342309", ph.response());
+    }
+    else { 
+        status = ret::A_FAIL_INVALID_POST_ID;
+    }
+    return status;
+}
+
 
 }//namespace
--- src/postfolderstrategy.h
+++ src/postfolderstrategy.h
@@ -13,6 +13,9 @@ class CredentialsManager;
 
 class PostFolderStrategy : public HttpStrategyInterface {
     int CreateFolderPost(Folder& folder, std::string& id_out);
+    int RetrieveFolderPost(const std::string& post_id, FolderPost& out);
+    bool UpdateFolderPost(Folder& folder, const std::string post_id);
+    int PostFolderPost(const std::string& post_id, FolderPost& fp);
 public:
     PostFolderStrategy() {}
     ~PostFolderStrategy() {}
