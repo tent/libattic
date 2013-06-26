21352d35a9c4dd4ec74ce3c88538c8f7baa46dc4
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-06-25 23:09:57 -0400

    wip

--- src/fileuploader.cpp
+++ src/fileuploader.cpp
@@ -1,5 +1,7 @@
 #include "fileuploader.h"
 
+namespace attic { 
+
 FileUplaoder::FileUploader(const std::string& filepath, 
                            const std::string& posts_feed, 
                            const AccessToken& at) {
@@ -10,3 +12,17 @@ FileUploader::~FileUploader() {
 
 }
 
+
+void FileUploader::BeginRequest() {
+
+}
+
+void FileUpload::ProcessFile() {
+
+}
+
+void FileUpload::EndRequest(Response& out) {
+
+}
+
+} //namespace
--- src/fileuploader.h
+++ src/fileuploader.h
@@ -3,21 +3,29 @@
 #pragma once
 
 #include <string>
+#include "downloadpost.h"
 #include "connection.h"
 #include "accesstoken.h"
+#include "response.h"
 
 
 namespace attic { 
 
 // Streams a file into a tent post
 class FileUploader {
+    bool GenerateDownloadPost(const std::string& filepath, DownloadPost& out);
 public:
     FileUploader(const std::string& filepath, 
                  const std::string& posts_feed, 
                  const AccessToken& at);
 
     ~FileUploader();
+
+    void BeginRequest();
+    void ProcessFile();
+    void EndRequest(Response& out);
 private:
+    DownloadPost dlp_;
     Connection* con_;
 
     std::string posts_feed_;
