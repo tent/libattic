5611bb2b50de280dcc0e6678b34dbdbb5a9db14d
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-06-27 22:52:38 -0400

    download post fix typo

--- src/downloadpost.cpp
+++ src/downloadpost.cpp
@@ -15,7 +15,7 @@ DownloadPost::~DownloadPost() {
 void DownloadPost::Serialize(Json::Value& root) {
     Json::Value content(Json::objectValue);
     content["filename"] = filename_;
-    content["file_size"] = file_size_;
+    content["filesize"] = filesize_;
     content["plaintext_hash"] = plaintext_hash_;
 
 
@@ -28,7 +28,7 @@ void DownloadPost::Deserialize(Json::Value& root) {
     Json::Value content;
     get_content("file_content", content);
     filename_           = content.get("filename", "").asString();
-    file_size_          = content.get("file_size", "").asString();
+    filesize_           = content.get("filesize", "").asString();
     plaintext_hash_     = content.get("plaintext_hash", "").asString();
 }
 
--- src/downloadpost.h
+++ src/downloadpost.h
@@ -14,16 +14,16 @@ public:
     void Serialize(Json::Value& root);  
     void Deserialize(Json::Value& root);
 
-    const std::string& filename() const         { return filename_; }
-    const std::string& file_size() const        { return file_size_; } 
-    const std::string& plaintext_hash() const    { return plaintext_hash_; }
+    const std::string& filename() const             { return filename_; }
+    const std::string& filesize() const             { return filesize_; } 
+    const std::string& plaintext_hash() const       { return plaintext_hash_; }
 
     void set_filename(const std::string& filename)      { filename_ = filename; }
-    void set_file_size(const std::string& file_size)    { file_size_ = file_size; }
-    void set_plaintext_hash(const std::string& hash)     { plaintext_hash_ = hash; }
+    void set_filesize(const std::string& filesize)      { filesize_ = filesize; }
+    void set_plaintext_hash(const std::string& hash)    { plaintext_hash_ = hash; }
 private:
     std::string filename_;
-    std::string file_size_;
+    std::string filesize_;
     std::string plaintext_hash_;
 };
 
