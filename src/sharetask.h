0b2a71c6ec84c94551884990f825d514a0804081
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-07-16 16:41:47 -0400

    prepares shared file post to share with a given entity

--- src/sharetask.h
+++ src/sharetask.h
@@ -2,12 +2,19 @@
 #define SHARETASK_H_
 #pragma once
 
+#include <string>
 #include "tenttask.h"
+#include "credentials.h"
 
 namespace attic {
 
 class ShareTask : public TentTask {
-    bool RerievePublicKey(const std::string& entity, std::string& out);
+    bool RetrievePublicKey(const std::string& entity_url, std::string& out);
+    bool RetrieveDecryptedCredentials(const std::string& post_id, Credentials& out);
+    bool CreateSharedFilePost(const std::string& file_post_id,
+                              const std::string& entity_url,
+                              const std::string& public_key);
+
 public:
     ShareTask(FileManager* fm, 
               CredentialsManager* cm,
